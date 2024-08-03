a364578f6d827ea65633f43c4f4c45f29aef0afc
Khaled Jalal Al-arashi <77218749+k-j-alarashi@users.noreply.github.com>
2024-08-04 00:07:30 +0300

    Add files via upload

---
+++ Lab2/src/lab2/Nested_IF.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author khale
+ */
+public class Nested_IF {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the Numbers : ");
+        int x = in.nextInt();
+        int y = in.nextInt();
+        int z = in.nextInt();
+        if(x>y){
+            if(x>z){
+                System.out.println(x);
+            }
+            else{
+                System.out.println(z);
+            }
+        }
+        else{
+            if(y>z){
+                System.out.println(y);
+            }
+            else{
+                System.out.println(z);
+            }
+        }
+    }
+}
